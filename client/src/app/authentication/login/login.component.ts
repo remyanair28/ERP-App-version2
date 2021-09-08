de007ad050b4ddcffa877c42030846a994518106
remyanair28 <35579798+remyanair28@users.noreply.github.com>
2021-09-08 16:16:57 -0400

    Add files via upload

---
+++ client/src/app/authentication/login/login.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormArray, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { LoginUser } from 'src/app/models/LoginUser';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public employeeForm;
+
+  constructor(private fb: FormBuilder, private router: Router,private authSerice: AuthService) { }
+
+  ngOnInit(): void {
+    this.employeeForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+    });
+  }
+
+  get email() {
+    return this.employeeForm.get('email');
+  }
+  get password() {
+    return this.employeeForm.get('password');
+  }
+
+  onSubmit(employeeForm){
+    console.log(this.employeeForm.value);
+    let loginUser: LoginUser = new LoginUser();
+    this.authSerice.login(this.employeeForm.value).subscribe(
+      (data) => {
+        loginUser.email = this.employeeForm.value.email
+        loginUser.password = this.employeeForm.value.password
+        loginUser.accesstoken = data['accessToken']
+        this.authSerice.getRole(this.employeeForm.value.email).subscribe(
+          (data) => {
+            // console.log(data)
+            if(data[0]!=undefined) {
+              let user = data[0];
+                console.log(user['role'])
+                loginUser.role = user['role']
+                loginUser.firstname = user['firstname']
+                loginUser.lastname = user['lastname']
+                loginUser.id = user['id']
+                localStorage.setItem('currentuser',JSON.stringify(loginUser))
+                this.authSerice.currentuser = JSON.parse(localStorage.getItem('currentuser'))
+                this.authSerice.loggedIn.next(true);
+                this.employeeForm.reset();
+                this.router.navigate(['/dashboard/eventslist']);
+            }
+          })
+      }        
+      ,
+      (error) => {console.log(error)}
+    )
+    
+  }
+}
