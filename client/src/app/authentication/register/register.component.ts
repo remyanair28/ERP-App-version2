f6bc24a0b16e88108293fbd17a5b3fecb2a313ea
remyanair28 <35579798+remyanair28@users.noreply.github.com>
2021-09-08 16:17:21 -0400

    Add files via upload

---
+++ client/src/app/authentication/register/register.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { UsersService } from 'src/app/services/users.service';
+import { User } from 'src/app/models/user';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  public userForm;
+  errorMsg: any;
+  users: User[];
+
+  constructor(private fb: FormBuilder, private router: Router, public UsersServ: UsersService, public authServ: AuthService) { }
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(3)]],
+      lastname: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      role: ['1', [Validators.required]]
+    });
+  }
+
+  get firstname() {
+    return this.userForm.get('firstname');
+  }
+
+  get lastname() {
+    return this.userForm.get('lastname');
+  }
+
+  get email() {
+    return this.userForm.get('email');
+  }
+
+  get password() {
+    return this.userForm.get('password');
+  }
+
+  get role() {
+    return this.userForm.get('role');
+  }
+
+  onSubmit(userForm){
+    console.log(this.userForm.value);
+    this.authServ.register(this.userForm.value).subscribe(
+      (data) => {
+        this.authServ.users.push(data); 
+        // this.UsersServ.postUsers(this.userForm.value).subscribe(
+        //   (data) => {this.users = data; console.log(this.users)},
+        //   (error) => this.errorMsg = error
+        // )
+      },
+      (error) => {this.errorMsg = error; console.log(this.errorMsg)}
+    )
+    this.router.navigate(['/login']);
+    this.userForm.reset();
+  }
+
+  login(){
+    this.router.navigate(['/login']);
+  }
+
+}
