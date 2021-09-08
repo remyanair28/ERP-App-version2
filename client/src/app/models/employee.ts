93535e9ab7d00e3f33386d9df409010db614dbfb
remyanair28 <35579798+remyanair28@users.noreply.github.com>
2021-09-08 16:22:52 -0400

    Add files via upload

---
+++ client/src/app/models/employee.ts
@@ -0,0 +1,6 @@
+export class Employee {
+    public id: number;
+    public name: string;
+    public age: number;
+    public salary: number;
+}
---
+++ client/src/app/models/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    public id:	number;
+    public firstname :	string;
+    public lastname: string;
+    public password: string;
+    public email :	string;
+    public role : number
+}
